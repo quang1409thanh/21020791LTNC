261aee64c0093bcd01babadc54b5085876cf2714
quang1409thanh <97092690+quang1409thanh@users.noreply.github.com>
2022-04-02 18:31:03 +0700

    commitb10

--- week7/10.cpp
+++ week7/10.cpp
@@ -1,7 +1,7 @@
 
 void rFilter(char *s){
     int pos=strlen(s);
-    for(int i=pos-1;i>=0;i--){
+    for(int i=pos-1;i>=0;--i){
         if((s[i]>='A'&&s[i]<='Z')||(s[i]>='a'&&s[i]<='z')) continue;
         s[i]='_';
     }
